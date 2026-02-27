f8f7b84d8fe56773676cfa456eb08c1729d40420
sudhirskp <gnitwallah@gmail.com>
2026-02-27 21:29:08 +0530

    add MergeSort and QuickSort classes for sorting arrays

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,60 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[low + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = low;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 5, 6, 18, 1, 2, 35 };
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/Sorting/QuickSort.java
@@ -0,0 +1,43 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static int[] quickSort(int[] arr) {
+        quickSortHelper(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private static void quickSortHelper(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quickSortHelper(arr, low, pivotIndex - 1);
+            quickSortHelper(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 6, 18, 1, 2, 35};
+        System.out.println(Arrays.toString(quickSort(arr)));
+    }
+}
